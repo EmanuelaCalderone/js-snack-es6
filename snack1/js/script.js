87691c7ac558a181721af26627ff2ca82af4e4d6
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-01-16 15:14:32 +0100

    create folders + create array exercise n.1

---
+++ snack1/js/script.js
@@ -0,0 +1,48 @@
+/* Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
+ */
+
+//creo l'array di oggetti (10)
+const biciclette = [
+    {
+        'nome': 'bici1',
+        'peso': '6'
+    },
+    {
+        'nome': 'bici2',
+        'peso': '7',
+    },
+    {
+        'nome': 'bici3',
+        'peso': '8',
+    },
+    {
+        'nome': 'bici4',
+        'peso': '6.5',
+    },
+    {
+        'nome': 'bici5',
+        'peso': '7.5',
+    },
+    {
+        'nome': 'bici6',
+        'peso': '5.9',
+    },
+    {
+        'nome': 'bici7',
+        'peso': '7.4'
+    },
+    {
+        'nome': 'bici8',
+        'peso': '6.1',
+    },
+    {
+        'nome': 'bici9',
+        'peso': '9',
+    },
+    {
+        'nome': 'bici10',
+        'peso': '8',
+    }
+];
+
+//creo condizione per far stampare a schermo la bici col minor peso
