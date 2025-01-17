7326d91168d18aacdfe23f17288ed5ebba0aa940
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-01-17 11:55:42 +0100

    fix bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,27 +1,18 @@
 /* Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri. */
 
-//creo la funzione con tre parametri in ingresso (array iniziale e due numeri)
-function middle(numeri, num1, num2) {
 
-    //inizializzo un secondo array vuoto
-    let numeriMiddle = [];
+// dichiaro l'array
+const saluti = ['ciao', 'buongiorno', 'buonasera', 'salve', 'salute', 'arrivederci'];
 
-    //itero tutti i numeri del primo array finché il numero successivo non sarà minore del secondo numero
-    for (let i = 0; i < num2; i++){
+// creo la funzione con tre parametri in ingresso (array e due numeri)
+function middle(array, min, max) {
+    // creo la condizione
+    for (let i = ++min; i < array.length; i = i++) {
+        //slice per far ritornare una parte dell'array
+        return array.slice(min, max);
+    };
+}
 
-        //creo una variabile che conterrà i valori successivi incrementandoli di uno a ogni iterazione
-        let nextNum = ++num1;
-        if (nextNum < num2) {
-            //pusho tutti quei numeri nel secondo array
-            numeriMiddle.push(nextNum);
-        };
-    } //ritorno il secondo array
-    return numeriMiddle;
-} 
+// test
+console.log(middle(saluti, 0, 4));
 
-//test 
-
-//creo array vuoto
-let numeri = [];
-//stampo e chiamo la funzione
-console.log(middle(numeri, 2, 10));
